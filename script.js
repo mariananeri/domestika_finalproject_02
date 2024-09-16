4d3fa0af3bdb773098950dc913f99764ba71a2ce
//mariana neri <150410791+mariananeri@users.noreply.github.com>
2024-09-16 18:09:32 -0400

    JS works

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 const canvas = document.querySelector("#drawingCanvas");
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const dan = document.createElement("img");
+const rifke = document.createElement("img");
+
+dan.src = "images/dan.png";
+rifke.src = "images/rifke.png";
+
+let paintbrush = dan;
+
+const context = canvas.getContext("2d");
+
+const handleMouseMove = (event) => {
+  const left = event.clientX;
+  const top = event.clientY;
+
+  context.drawImage(paintbrush, left, top);
+};
+
+const handleClick = () => {
+  if (paintbrush === dan) {
+    paintbrush = rifke;
+  } else {
+    paintbrush = dan;
+  }
+};
+
+canvas.addEventListener("click", handleClick);
+canvas.addEventListener("mousemove", handleMouseMove);
